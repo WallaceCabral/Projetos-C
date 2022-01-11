40358f2176cfd2d77e11cb82cbb46abd5ba8b942
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-01-11 12:46:53 -0300

    Valor do Raio
    Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
    casas decimais conforme exemplos.
    Fórmula da área: area = π . raio2
    Considere o valor de π = 3.14159

---
+++ ValorDoRaio/ValorDoRaio/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+float pi = (float)3.14159;
+float area,num1;
+
+Console.WriteLine("Digite o valor do Raio: ");
+num1 = float.Parse(Console.ReadLine());
+
+area = pi * (num1*num1);
+
+Console.WriteLine($"o valor da área deste círculo "+area.ToString("F4"));
+
