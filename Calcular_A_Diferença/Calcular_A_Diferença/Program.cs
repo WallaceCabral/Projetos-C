bcacfaa08de185059c67601e0d503592f5113a16
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-01-11 12:52:08 -0300

    Calcular a diferença
    Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
    de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).

---
+++ Calcular_A_Diferença/Calcular_A_Diferença/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+int A, B, C, D, DIFERENCA;
+
+Console.WriteLine("Digite os 4 valores para fazer o calculo da diferença");
+A = int.Parse(Console.ReadLine());
+B = int.Parse(Console.ReadLine());
+C = int.Parse(Console.ReadLine());
+D = int.Parse(Console.ReadLine());
+
+DIFERENCA = ((A * B) - (C * D));
+
+Console.WriteLine("A diferença entre os numero foi : "+ DIFERENCA);
