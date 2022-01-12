8cfaa855286b1fb0b4dd7ff256bfde88306f4ef5
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-01-12 10:29:33 -0300

    Calculo de salario
    Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
    hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
    decimais.

---
+++ CalculoDeSalario/CalculoDeSalario/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+int num,horas;
+float valorHora,salario;
+
+Console.WriteLine("Digite seu numero de funcionario");
+num =int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite quantas horas foram trabalhadas");
+horas = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor da hora trabalhada");
+valorHora = float.Parse(Console.ReadLine());
+
+salario = (float)horas * valorHora;
+
+Console.WriteLine($"Numero: {num}, seu salario foi de R${salario.ToString("F2")}");
