2b5bc10232cbfcaa7d198f141a11a982bdf7ae24
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-01-13 10:10:10 -0300

    EstoqueProduto
    Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.

---
+++ EstoqueProduto/EstoqueProduto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+int cod1, cod2, qte1, qte2;
+double preco1, preco2, total;
+
+Console.WriteLine("Digite o Codigo, quantidade e valor do produto nesta sequencia ");
+
+string[] valores = Console.ReadLine().Split(' ');
+cod1 = int.Parse(valores[0]);
+qte1 = int.Parse(valores[1]);
+preco1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+valores = Console.ReadLine().Split(' ');
+cod2 = int.Parse(valores[0]);
+qte2 = int.Parse(valores[1]);
+preco2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+total = preco1 * qte1 + preco2 * qte2;
+
+Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
