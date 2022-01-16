03cf13242eb1b1f805920ee18dea352cb9affbbe
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-01-16 17:59:41 -0300

    Teste de Senha
    Programa para testar login

---
+++ TesteDeSenha/TesteDeSenha/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+int senha = 123456, contador =0,check=0;
+string login;
+
+Console.WriteLine("Entre com seu login");
+login = Console.ReadLine();
+
+
+while (senha != 123456 || contador != 3)
+{
+    Console.WriteLine("Digite sua senha:");
+    check = int.Parse(Console.ReadLine()); 
+
+    contador++;
+}
+if (check == senha)
+{
+    Console.WriteLine("Logado com Sucesso");
+}
+else
+    Console.WriteLine("Usuário bloqueado por tentativa de senha incorretas");
