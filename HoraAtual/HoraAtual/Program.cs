61c89afcea187fd219bbc90279f1ac4adc7ac7bd
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-01-16 18:36:23 -0300

    atualizado
    atualizado

--- HoraAtual/HoraAtual/Program.cs
+++ HoraAtual/HoraAtual/Program.cs
@@ -3,3 +3,6 @@
 string datetime = DateTime.Now.ToString("HH:mm:ss tt");
 Console.WriteLine("A hora atual é : "+datetime);
 
+Double epoch = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+
+Console.WriteLine(epoch);
