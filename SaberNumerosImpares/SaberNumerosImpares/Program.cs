f3ebb585033ffac0bdd0ded1c03f592a3ddf7b44
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-01-30 17:06:15 -0300

    SaberNumerosImpares
    Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
    X, se for o caso.

---
+++ SaberNumerosImpares/SaberNumerosImpares/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+X, se for o caso.*/
+
+int num;
+
+Console.WriteLine("Digite um valor");
+num =int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= num; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+
+}
