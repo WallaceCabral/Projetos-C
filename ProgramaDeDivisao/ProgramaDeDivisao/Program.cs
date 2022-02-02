420721212bfac3bde3ee055bf56145a9711910de
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-02-02 11:21:56 -0300

    ProgramaDeDivisao
    Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
    segundo. Se o denominador for igual a zero, mostrar a mensagem "divisão impossível".

---
+++ ProgramaDeDivisao/ProgramaDeDivisao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+Console.WriteLine("Digite quantos vezes deseja usar o programa ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Digite os valores primeiro o numerador e seguido do denominador ");
+
+    string[] line = Console.ReadLine().Split(' ');
+    int x = int.Parse(line[0]);
+    int y = int.Parse(line[1]);
+
+    if (y == 0)
+    {
+        Console.WriteLine("divisao impossivel");
+    }
+    else
+    {
+        double div = (double)x / y;
+        Console.WriteLine("O resultado é: "+div.ToString("F1", CultureInfo.InvariantCulture));
+    }
+}
