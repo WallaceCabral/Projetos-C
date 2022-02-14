0fbac0a21981d9e5347e44daeac7a75e990e8c57
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-02-14 11:34:11 -0300

    TrianguloQualquer
    Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas
    válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos
    possui a maior área.
    A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a
    seguinte (fórmula de Heron):

---
+++ TrianguloQualquer/TrianguloQualquer/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas
+válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos
+possui a maior área.
+A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a
+seguinte (fórmula de Heron)*/
+
+using System;
+using System.Globalization;
+using TrianguloQualquer;
+
+Triangulo x, y;
+
+x = new Triangulo();
+y = new Triangulo();
+
+
+Console.WriteLine("Entre com as medidas do triangulo X:");
+x.A= double.Parse(Console.ReadLine());
+x.B= double.Parse(Console.ReadLine());
+x.C= double.Parse(Console.ReadLine());  
+
+Console.WriteLine("Entre com as medidas do trianguloy Y:");
+y.A= double.Parse(Console.ReadLine());  
+y.B= double.Parse(Console.ReadLine());
+y.C= double.Parse(Console.ReadLine());
+
+
+double p = (x.A + x.B + x.C) / 2.0;
+double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+p = (y.A + y.B + y.C) / 2.0;
+double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+if (areaX > areaY)
+{
+    Console.WriteLine("Maior área: X");
+}
+else
+{
+    Console.WriteLine("Maior área: Y");
+}
