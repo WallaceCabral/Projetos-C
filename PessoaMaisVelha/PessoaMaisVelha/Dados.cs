5ca35784b9e64bf37b620ca73e201d9ee3392cd9
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-02-14 13:00:33 -0300

    PessoaMaisVelha
    Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais
    velha.

---
+++ PessoaMaisVelha/PessoaMaisVelha/Dados.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PessoaMaisVelha
+{
+    internal class Dados
+    {
+        public string nome;
+        public int idade;
+    }
+}
---
+++ PessoaMaisVelha/PessoaMaisVelha/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais
+velha.*/
+using PessoaMaisVelha;
+
+Dados nome, idade;
+
+
+Dados p1 = new Dados();
+Dados p2 = new Dados();
+
+
+
+Console.WriteLine("Digite o nome da 1° pessoa:");
+p1.nome = Console.ReadLine();
+
+Console.WriteLine("Digite a idade da 1° pessoa:");
+p1.idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o nome da 2° pessoa:");
+p2.nome = Console.ReadLine();
+
+Console.WriteLine("Digite a idade da 2° pessoa:");
+p2.idade = int.Parse(Console.ReadLine());
+
+
+if(p1.idade > p2.idade)
+{
+    Console.WriteLine("O mais velho é: " + p1.nome);
+}
+else
+{
+    Console.WriteLine("O mais velho é: " + p2.nome);
+}
