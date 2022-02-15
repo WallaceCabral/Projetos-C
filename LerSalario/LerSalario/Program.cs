0c46420ff8b6d50fceda80149cb0dd1207c9428e
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-02-15 11:31:38 -0300

    LerSalario
    Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
    médio dos funcionários.

---
+++ LerSalario/LerSalario/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
+médio dos funcionários.*/
+using LerSalario;
+
+
+Empresa p1, p2;
+p1 = new Empresa();
+p2 = new Empresa();
+
+
+Console.WriteLine("Digite o NOME do 1° funcionario:");
+p1.nome = Console.ReadLine();
+Console.WriteLine("Digite o SALARIO do 1° funcionario:");
+p1.salario = double.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("Digite o NOME do 2° funcionario:");
+p2.nome = Console.ReadLine();
+Console.WriteLine("Digite o SALARIO do 2° funcionario:");
+p2.salario = double.Parse(Console.ReadLine());
+
+double media = (p1.salario + p2.salario)/ 2;
+
+Console.WriteLine("Salario medio é de R$ "+media);
