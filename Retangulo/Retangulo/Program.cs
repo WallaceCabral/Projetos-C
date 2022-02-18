2ee3e2bb4cf62690cf003e17e49cb1d8b7b04112
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-02-17 22:45:13 -0300

    Retangulo
    Programa para calcular a diagonal, área, perímetro de um retângulo

---
+++ Retangulo/Retangulo/Program.cs
@@ -0,0 +1,11 @@
+﻿using Retangulo;
+
+Formula ret = new Formula();
+
+Console.WriteLine("Entre com a largura e altura do retangulo:");
+ret.largura = double.Parse(Console.ReadLine()); 
+ret.altura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Area: " +ret.Area()) ;
+Console.WriteLine("Perimetro: " + ret.Perimetro());
+Console.WriteLine("Diagonal: " + ret.Diagonal());
