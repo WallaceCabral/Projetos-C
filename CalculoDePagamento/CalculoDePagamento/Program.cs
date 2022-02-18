606b581576df5a5500bf4b06718ac9cda2123a27
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-02-18 11:42:43 -0300

    Calculo De Pagamento
    Fazer um programa para ler os dados de um funcionário (nome,
    salário bruto e imposto). Em seguida, mostrar os dados do
    funcionário (nome e salário líquido). Em seguida, aumentar o salário
    do funcionário com base em uma porcentagem dada (somente o
    salário bruto é afetado pela porcentagem) e mostrar novamente os
    dados do funcionário

---
+++ CalculoDePagamento/CalculoDePagamento/Program.cs
@@ -0,0 +1,28 @@
+﻿using CalculoDePagamento;
+
+Funcionario F;
+
+F = new Funcionario();
+float porcent; 
+
+Console.WriteLine("Entre com os dados do funcionario");
+Console.Write("Nome: ");
+F.Nome = Console.ReadLine();
+
+Console.WriteLine();
+Console.Write("Salario Bruto: ");
+F.SalarioBruto = float.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.Write("Imposto: ");
+F.Imposto = float.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.Write("Digite a porcentagem de aumento: ");
+porcent = float.Parse(Console.ReadLine());
+
+F.AumentorSalario(porcent);
+
+Console.WriteLine();
+Console.WriteLine("Dados atualizados: " + F);
+
