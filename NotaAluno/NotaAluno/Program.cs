2813cf12319af021bac8e2af0dc78eb7b43e504b
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-02-18 12:07:36 -0300

    Nota Aluno
    Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano
    (primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no
    ano. Dizer também se o aluno está APROVADO ou REPROVADO e, em caso negativo, quantos pontos faltam
    para o aluno obter o mínimo para ser aprovado (que é 60 pontos).

---
+++ NotaAluno/NotaAluno/Program.cs
@@ -0,0 +1,30 @@
+﻿using NotaAluno;
+using System.Globalization;
+
+DadosAluno Aluno;
+Aluno = new DadosAluno();
+
+
+Console.Write("Qual o nome do aluno? ");
+Aluno.Nome = Console.ReadLine();
+
+Console.WriteLine();
+Console.Write("Digite as notas: ");
+Aluno.Nota1 = float.Parse(Console.ReadLine());
+Aluno.Nota2 = float.Parse(Console.ReadLine());
+Aluno.Nota3 = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Nota Final = "+Aluno.NotaFinal().ToString("F2",CultureInfo.InvariantCulture));
+
+if (Aluno.Aprovado())
+{
+    Console.WriteLine("Aprovado");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+    Console.WriteLine("Faltaram: "+Aluno.NotaRestante()+" pontos");
+}
+
+
