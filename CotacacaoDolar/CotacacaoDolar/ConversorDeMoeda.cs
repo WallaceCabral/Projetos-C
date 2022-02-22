bb4bed8c77160cc1b7ff041b269a723d0c08a703
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-02-22 10:12:39 -0300

    Cotação Do Dolar
    Programa para converter o dólar em real

---
+++ CotacacaoDolar/CotacacaoDolar/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CotacacaoDolar
+{
+    internal class ConversorDeMoeda
+    {
+        public static float Iof = 6;
+
+        public static float cotacao(float qtdeDolar,float valorDoDolar)
+        {
+            float total = qtdeDolar * valorDoDolar;
+            return total + total * Iof / 100 ;
+        }
+    }
+}
---
+++ CotacacaoDolar/CotacacaoDolar/Program.cs
@@ -0,0 +1,11 @@
+﻿using CotacacaoDolar;
+
+Console.Write("Qual é a cotação atual do Dólar ?");
+float valorDoDolar = float.Parse(Console.ReadLine());
+
+Console.Write("Quantos dólares voce vai comprar? ");
+float qtdeDolar = float.Parse(Console.ReadLine());
+
+float result =ConversorDeMoeda.cotacao(valorDoDolar, qtdeDolar);
+
+Console.WriteLine("Valor a ser pago em reais = "+result);
