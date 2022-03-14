41f29ac0bec99af9b1c2af796c1a54dba90ccc8e
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-03-14 10:10:32 -0300

    Altura Media de Pessoas
    Fazer um programa para ler um número inteiro N e a altura de N pessoas.
    Armazene as N alturas em um vetor. Em seguida, mostrar a altura média dessas
    pessoas.

---
+++ AlturaMedia/AlturaMedia/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Fazer um programa para ler um número inteiro N e a altura de N pessoas.
+Armazene as N alturas em um vetor. Em seguida, mostrar a altura média dessas
+pessoas.*/
+
+using System.Globalization;
+
+double aux = 0,media;
+
+Console.WriteLine("Digite quantas alturas deseja ler: ");
+int n = int.Parse(Console.ReadLine());
+
+double[] vect = new double[n]; // criando um vetor de 'N' posições;
+
+Console.WriteLine("Digite as alturas:");
+for (int i = 0; i < n; i++)
+{
+    vect [i] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture); 
+    aux =aux + vect [i];
+}
+
+media = aux / n;
+
+Console.WriteLine("A media das Alturas é: " +media.ToString("F2", CultureInfo.InvariantCulture));
+
