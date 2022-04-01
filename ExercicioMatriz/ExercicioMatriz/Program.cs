39d468940b361127abb928e9c73008b1e5fdc4e3
Wallace Cabral <wallacecabral.carlos@gmail.com>
2022-04-01 11:35:07 -0300

    ExercicioMatriz
    Fazer um programa para ler dois números inteiros M e N, e depois ler
    uma matriz de M linhas por N colunas contendo números inteiros,
    podendo haver repetições. Em seguida, ler um número inteiro X que
    pertence à matriz. Para cada ocorrência de X, mostrar os valores à
    esquerda, acima, à direita e abaixo de X, quando houver, conforme
    exemplo.

---
+++ ExercicioMatriz/ExercicioMatriz/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Fazer um programa para ler um número inteiro N e uma matriz de
+ordem N contendo números inteiros. Em seguida, mostrar a diagonal
+principal e a quantidade de valores negativos da matriz.*/
+
+using System;
+
+Console.Write("Digite quantos valores serão lidos: ");
+int n = int.Parse(Console.ReadLine());
+
+int []  vet = new int[n];
+int [,] mat = new int[n,n]; //criação da matriz na memoria 
+
+Console.WriteLine("Digite a matriz separando cada linha por espaço: ex:1 2 3");
+for (int i = 0; i < n; i++)
+{
+    
+    string[] values = Console.ReadLine().Split(' ');// aqui esta recebendo o numero de forma de STRING;
+    for (int j = 0; j < n; j++)
+    {
+        mat[i,j] = int.Parse(values[j]); // aqui estamos convertendo o valor recebido pelo VALUES na posição J;
+              
+    }
+}
+Console.WriteLine("Diagonal principal: ");
+for(int i = 0; i < n; i++)
+{
+    Console.WriteLine(mat[i,i]+ " "); // aqui ele pega a posição da diagonal
+   
+}
