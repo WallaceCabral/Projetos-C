b781097a6ac6d3a70ff78d9404a7f763ca101405
Wallace Cabral <43008367+WallaceCabral@users.noreply.github.com>
2022-10-09 17:55:59 -0300

    Condicional Ternária

---
+++ Condicional_Ternaria/Condicional_Ternaria/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Condicional Ternária");
+
+Console.WriteLine("Qual o valor do preço ?");
+double preco = double.Parse(Console.ReadLine());
+
+
+double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+
+Console.WriteLine(desconto);
+
