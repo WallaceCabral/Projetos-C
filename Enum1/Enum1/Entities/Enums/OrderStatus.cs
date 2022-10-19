6e102b0ec8a2d47aa3fef96abbb45ad619060242
Wallace Cabral <43008367+WallaceCabral@users.noreply.github.com>
2022-10-19 19:26:01 -0300

    Enum

---
+++ Enum1/Enum1/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Enum1.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendiPayment = 0, //pagamento pendente
+        Processing = 1,  // processando
+        Shipped = 2,     //enviado
+        Delivered = 3    // entregue
+    }
+}
---
+++ Enum1/Enum1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Enum1.Entities;
+using Enum1.Entities.Enums;
+
+Order order = new Order
+{
+    Id = 1080,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendiPayment
+};
+
+Console.WriteLine(order);
+string txt= OrderStatus.PendiPayment.ToString();
+
+OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+Console.WriteLine(os);
