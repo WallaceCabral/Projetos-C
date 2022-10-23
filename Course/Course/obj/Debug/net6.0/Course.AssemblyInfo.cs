17ced4cb60a2e33c165b0d93c444f8dc51486ee6
Wallace Cabral <43008367+WallaceCabral@users.noreply.github.com>
2022-10-22 22:09:34 -0300

    Produto

--- Course/Course/obj/Debug/net6.0/Course.AssemblyInfo.cs
+++ Course/Course/obj/Debug/net6.0/Course.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("course")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Course")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("course")]
-[assembly: System.Reflection.AssemblyTitleAttribute("course")]
+[assembly: System.Reflection.AssemblyProductAttribute("Course")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Course")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
---
+++ Produto/Produto/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Produto.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public Double Price { get; set; }
+
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
