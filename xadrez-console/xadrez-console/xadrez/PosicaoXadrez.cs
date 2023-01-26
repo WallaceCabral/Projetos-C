97ab58149c4404916a5534a8d064ab827f6488c3
Wallace Cabral <43008367+WallaceCabral@users.noreply.github.com>
2023-01-26 11:46:20 +0044

    xadrez
    mais algumas alterações

---
+++ xadrez-console/xadrez-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+     class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao() {
+            return new Posicao('a' - coluna, 8 - linha);
+ }
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
